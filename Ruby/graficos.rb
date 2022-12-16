8dd2ed50432d4b22f6b1559b573990fa68c5c95e
ElNico56 <meeqnico@gmail.com>
2022-12-16 08:28:04 -0300

    detalles

--- Ruby/graficos.rb
+++ Ruby/graficos.rb
@@ -4,33 +4,33 @@ set title: "Hello Triangle"
 set width: 1280, height: 720
 
 tri = Triangle.new(
-  x1: 320, y1: 50,
-  x2: 540, y2: 430,
-  x3: 100, y3: 430,
-  color: ["#ff0000", "#00ff00", "#0000ff"],
+x1: 320, y1: 50,
+x2: 540, y2: 430,
+x3: 100, y3: 430,
+color: ["#ff0000", "#00ff00", "#0000ff"],
 )
 
 on :key do |event|
-  # puts event.key
-  case event.key
-  when "up"
-    tri.y1 -= 10
-    tri.y2 -= 10
-    tri.y3 -= 10
-  when "left"
-    tri.x1 -= 10
-    tri.x2 -= 10
-    tri.x3 -= 10
-  when "down"
-    tri.y1 += 10
-    tri.y2 += 10
-    tri.y3 += 10
-  when "right"
-    tri.x1 += 10
-    tri.x2 += 10
-    tri.x3 += 10
-  else
-  end
+	# puts event.key
+	case event.key
+	when "up"
+		tri.y1 -= 10
+		tri.y2 -= 10
+		tri.y3 -= 10
+	when "left"
+		tri.x1 -= 10
+		tri.x2 -= 10
+		tri.x3 -= 10
+	when "down"
+		tri.y1 += 10
+		tri.y2 += 10
+		tri.y3 += 10
+	when "right"
+		tri.x1 += 10
+		tri.x2 += 10
+		tri.x3 += 10
+	else
+	end
 end
 
 update do
