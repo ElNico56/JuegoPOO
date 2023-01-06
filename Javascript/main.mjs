6a81bbd982d7887b76c69a1f9a2d6e9a30e05508
ElNico56 <meeqnico@gmail.com>
2023-01-05 21:45:46 -0300

    Stuff

--- Javascript/main.mjs
+++ Javascript/main.mjs
@@ -1,6 +1,15 @@
 import { Ejemplo } from './classes.mjs'; // importar clase
+import readline from './imported_modules.js' // leer linea
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
 
 let objeto = new Ejemplo("a", "b"); // instancia
 objeto.print(); // metodo
 console.log(objeto.atributo_publico); // accesible porque es publico
 console.log(objeto.atributo_privado); // retorna undefined
+
+let userInput = "";
+rl.question("What is your name\n", function (string) { userInput = string; rl.close(); });
