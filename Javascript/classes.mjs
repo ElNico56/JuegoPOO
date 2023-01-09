b0e99d680b6b3ead9796036351eeb858c9a0a921
ElNico56 <meeqnico@gmail.com>
2023-01-09 04:26:11 -0300

    Stuff

--- Javascript/classes.mjs
+++ Javascript/classes.mjs
@@ -1,8 +1,10 @@
-function randomInt(min, max) { // retorna un entero entre min y max inclusivo
+import { throws } from "assert";
+
+export function randomInt(min, max) { // retorna un entero entre min y max inclusivo
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function choose(list) { // elegir un elemento random
+export function choose(list) { // elegir un elemento random
 	return list[Math.floor(Math.random() * list.length)]
 }
 
@@ -11,37 +13,35 @@ for (let i = 0; i <= (Date.now() % 100); i++) {
 } // dejamos que random se randomize :v
 
 export function nameGenerator() {
-	const consonants = ["M", "N", "P", "T", "K", "S", "W", "L", "J", "CH", "R", "KR", "TR", "PR"];
-	const vowels = ["A", "E", "I", "O", "U"];
+	const consonants = ["M", "N", "P", "T", "K", "S", "W", "L", "J", "CH", "R", "TR", "PR"];
+	const vowels = ["A", "E", "I", "O", "U", "AI", "EI", "OU", "OI"];
 	let name = "";
-	for (let i = 0; i < randomInt(2, 4); i++) {
+	for (let i = 0; i < randomInt(2, 3); i++) {
 		name += choose(consonants) + choose(vowels);
-	}
-	if (Math.random() < 0.25) {
-		name += "-";
-		for (let i = 0; i < randomInt(1, 2); i++) {
-			name += choose(consonants) + choose(vowels);
-		}
 	}
 	return name;
 }
 
 export class Monster {
-	name;
+	#name;
 	element; // uno de [fuego, agua, planta]
 	#isBlocking;
 	#hp;
+	#maxHp;
 	#attack;
 	#defense;
 	#stamina;
-	constructor(name, element) {
-		this.name = name;
+	#maxStamina;
+	constructor(name, element, level = 0) {
+		this.#name = name;
 		this.element = element;
 		this.#isBlocking = false;
-		this.#hp = 100;
-		this.#attack = randomInt(5, 15);
-		this.#defense = randomInt(0, 5);
+		this.#hp = 100 + 25 * level;
+		this.#maxHp = 100 + 25 * level;
+		this.#attack = randomInt(5, 10) + level;
+		this.#defense = randomInt(5, 10) + level;
 		this.#stamina = 5;
+		this.#maxStamina = 10;
 	}
 	weakAttack(enemy) {
 		if (this.#stamina > 0) {
@@ -58,6 +58,7 @@ export class Monster {
 			enemy.damage(ammount);
 			this.#stamina -= 1;
 		}
+		this.#isBlocking = false;
 	}
 	strongAttack(enemy) {
 		if (this.#stamina > 1) {
@@ -74,10 +75,16 @@ export class Monster {
 			enemy.damage(ammount);
 			this.#stamina -= 2;
 		}
+		this.#isBlocking = false;
 	}
 	damage(amount) {
 		if (!this.#isBlocking) {
-			this.#hp -= amount + this.#defense;
+			this.#hp -= amount;
+			if (this.#hp < 0) {
+				this.#hp = 0;
+			}
+		} else {
+			this.#hp -= Math.max(0, amount - this.#defense);
 			if (this.#hp < 0) {
 				this.#hp = 0;
 			}
@@ -86,9 +93,37 @@ export class Monster {
 	}
 	block() {
 		this.#isBlocking = true;
+		this.#stamina -= 1;
 	}
-	rest() {
-		this.#stamina += 1;
-		this.#hp += 5;
+	rest(multiplier = 1) {
+		this.#stamina += 1 * multiplier;
+		this.#hp += 5 * multiplier;
+		if (this.#stamina > this.#maxStamina) {
+			this.#stamina = this.#maxStamina;
+		}
+		if (this.#hp > this.#maxHp) {
+			this.#hp = this.#maxHp;
+		}
+	}
+	status() {
+		let color = "";
+		if (this.element == "FUEGO") {
+			color = "\x1b[91m";
+		} else if (this.element == "PLANTA") {
+			color = "\x1b[92m";
+		} else {
+			color = "\x1b[94m";
+		}
+		console.log(`\n~+~ ${this.#name} ~+~`);
+		console.log(` - Elemento: ${color}${this.element}\x1b[0m`);
+		console.log(` - Ataque: ${this.#attack}`);
+		console.log(` - Defensa: ${this.#defense}`);
+	}
+}
+
+export class World {
+	#biome;
+	constructor(biome) {
+		this.#biome = biome
 	}
 }
--- Javascript/main.mjs
+++ Javascript/main.mjs
@@ -1,18 +1,56 @@
-import {Monster, nameGenerator} from './classes.mjs'; // importar clase
+import {Monster, nameGenerator, choose} from './classes.mjs'; // importar clase
 import fs from "fs"
+import { exit } from 'process';
 
-const prompt = msg => { // magia negra
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const prompt = msg => { // magia negra para pedir input
 	fs.writeSync(1, String(msg));
 	let s = '', buf = Buffer.alloc(1);
 	while (buf[0] - 10 && buf[0] - 13)
-		s += buf, fs.readSync(0, buf, 0, 1, 0);
+	s += buf, fs.readSync(0, buf, 0, 1, 0);
 	return s.slice(1);
 }
 
-// let userinput = prompt("weas: ");
+let team = [];
+team.push(new Monster(nameGenerator(), "FUEGO"));
+team.push(new Monster(nameGenerator(), "AGUA"));
+team.push(new Monster(nameGenerator(), "PLANTA"));
+team.push(new Monster(nameGenerator(), choose(["FUEGO", "AGUA", "PLANTA"])));
 
-console.log()
-for (let i = 0; i < 50; i++) {
-	console.log("	"+nameGenerator());
+console.log(`\x1b[96m
+██╗   ██╗███╗  ██╗███╗  ██╗ █████╗ ███╗   ███╗███████╗██████╗ 
+██║   ██║████╗ ██║████╗ ██║██╔══██╗████╗ ████║██╔════╝██╔══██╗
+██║   ██║██╔██╗██║██╔██╗██║███████║██╔████╔██║█████╗  ██║  ██║
+██║   ██║██║╚████║██║╚████║██╔══██║██║╚██╔╝██║██╔══╝  ██║  ██║
+╚██████╔╝██║ ╚███║██║ ╚███║██║  ██║██║ ╚═╝ ██║███████╗██████╔╝
+ ╚═════╝ ╚═╝  ╚══╝╚═╝  ╚══╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═════╝ 
+
+\x1b[0mUNNAMED es un juego de peleas por turnos donde controlas
+un equipo de 4 monstruos contra una serie de monstruos
+salvajes camino a tu destino.
+
+Tu objetivo como su guía es darle ordenes a tus monstruos
+para poder derrotar a cada monstruo salvaje.
+`);
+
+let option = parseInt(prompt("1. Modo historia\n2. Modo arena\n3. Sobre el juego\n4. Salir\n > "));
+
+switch (option) {
+	case 1:
+		console.log("\nTu equipo consiste de:")
+		for (let i = 0; i < team.length; i++) {
+			team[i].status();
+		}
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		exit();
+	default:
+	console.log("Opción invalida");
 }
-console.log()
+
+console.log("jiru jaru");
